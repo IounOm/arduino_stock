06679d6de17ea56af4005213e01741d02ac30834
IounOm <iounnirach15@gmail.com>
2021-10-26 12:58:48 +0700

    set mui & eslint react app

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
-import logo from './logo.svg';
-import './App.css';
+import './App.css'
+import Button from '@mui/material/Button'
+import logo from './logo.svg'
 
 function App() {
   return (
@@ -7,7 +8,11 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Edit
+          {' '}
+          <code>src/App.js</code>
+          {' '}
+          and save to reload.
         </p>
         <a
           className="App-link"
@@ -17,9 +22,10 @@ function App() {
         >
           Learn React
         </a>
+        <Button>Primary</Button>
       </header>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
