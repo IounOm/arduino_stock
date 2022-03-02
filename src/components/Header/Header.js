937639dfd632a034f7a26c96fd8ae6d3dea87234
IounOm <iounnirach15@gmail.com>
2022-03-02 18:53:14 +0700

    test console

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -81,6 +81,7 @@ const useStyles = makeStyles((theme) => ({
 
 function Header(props) {
   const { children } = props
+  console.log(55555)
   const classes = useStyles()
   const history = useHistory()
   const dispatch = useDispatch()
